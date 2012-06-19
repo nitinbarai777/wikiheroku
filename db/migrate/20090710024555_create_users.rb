c38534a5e3756119d555f8a240d4f56716d59356
nitin777 <nitin.barai777@gmail.com>
2012-06-19 11:51:27 +0530

    Update master

--- db/migrate/20090710024555_create_users.rb
+++ db/migrate/20090710024555_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.column :last_ip_address, :string, :null => false
       t.timestamps
     end
+    User.create({:email => "admin@wiki.com", :password => 'admin', :last_ip_address => "192.168.0.91"})
   end
 
   def self.down
